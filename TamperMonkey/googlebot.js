353874a86ac5a2a9995637f215275cdc68873f56
nesa402610 <argyleferris@gmail.com>
2022-04-20 12:21:29 +0300

    Ходит на сайт, кликается по нему. Переход на гугл на рандоме и повтор действий

--- TamperMonkey/googlebot.js
+++ TamperMonkey/googlebot.js
@@ -5,6 +5,7 @@
 // @description  try to take over the world!
 // @author       Ferris Argyle
 // @match        https://www.google.com/*
+// @match        https://napli.ru/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
@@ -18,6 +19,7 @@ const keywords = ["10 самых популярных шрифтов от Google
 let keyword = keywords[getRandom(0, keywords.length)];
 const input = document.querySelector('input');
 const submit = document.getElementsByName('btnK')[1];
+let links = document.links;
 if (submit) {
     let i = 0;
     const timerID = setInterval(() => {
@@ -28,19 +30,30 @@ if (submit) {
             submit.click();
         }
     }, 400);
+} else if (location.hostname === 'napli.ru') {
+    setInterval(() => {
+        if (getRandom(0, 100) >= 90) location.href = 'https://google.com'
+        let index = getRandom(0, links.length);
+        if (links[index].href.indexOf('napli.ru') !== -1) links[index].click();
+
+    }, getRandom(1500, 3000));
 } else {
     let nextPage = true;
-    let links = document.links;
 
     for (let i = 0; i < links.length; i++) {
         if (links[i].href.indexOf("napli.ru") !== -1) {
             let link = links[i];
+            link.removeAttribute('target')
             nextPage = false;
             setTimeout(() => link.click(), getRandom(1500, 4000));
             break;
         }
     }
-    if (nextPage) {
+    if (document.querySelector('.YyVfkd').innerText === '5') {
+        nextPage = false;
+        location.href = '/';
+    }
+    if (nextPage && document.getElementById('pnnext')) {
         setTimeout(() => {
             document.getElementById('pnnext').click();
         }, getRandom(1500, 4000));
