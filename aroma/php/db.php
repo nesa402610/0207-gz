b4560a9babeb22de0b215ee2809da050d29eae17
nesa402610 <argyleferris@gmail.com>
2022-05-06 12:32:56 +0300

    Новые роуты, дефолт коннект к бд, User::class. В классе регистрация и логин есть

---
+++ aroma/php/db.php
@@ -0,0 +1,2 @@
+<?php
+$mysqli = mysqli_connect("localhost", "vytspqpe_nesa402610", "R5SlFL8V", "vytspqpe_zerotwoGZ");
--- aroma/router.php
+++ aroma/router.php
@@ -4,6 +4,8 @@ header('Content-Type: text/html; charset=utf-8');
 $url = $_SERVER['REQUEST_URI'];
 $url = explode('/', $url);
 $link = $url[2];
+require_once("php/classes/User.php");
+require_once("php/db.php");
 
 if ($link == 'auth') {
     $content = file_get_contents("pages/login.html");
@@ -11,14 +13,20 @@ if ($link == 'auth') {
     $content = file_get_contents("pages/register.html");
 } else if ($link == 'blog') {
     $content = file_get_contents("pages/blog.html");
-}else if ($link == 'users') {
+} else if ($link == 'users') {
     require_once('pages/users/index.php');
+} else if ($link == 'addUser') {
+    echo User::addUser($_POST["name"], $_POST["lastname"], $_POST['email'], $_POST['pass']);
+//    var_dump($mysqli);
+} else if ($link == 'userLogin') {
+    echo User::authUser($_POST['email'], $_POST['pass']);
+//    var_dump($mysqli);
 } else {
     $content = file_get_contents("pages/index.php");
 }
 
 if (!empty($content))
-require_once('template.php');
+    require_once('template.php');
 //if ($link == 'blog') {
 //    require_once('blog.html');
 //} else if ($link == 'cart') {
