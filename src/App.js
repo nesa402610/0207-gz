36407b974c0427ca008fa228b769786c136dd43b
nesa402610 <argyleferris@gmail.com>
2022-05-12 12:03:07 +0300

    Тот самый профиль но теперь на реакте, реакт роутер дом, роуты, отдельные компоненты

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import {NavLink, Routes, Route} from "react-router-dom";
+import Profile from "./components/profile";
+import Messages from "./components/messages";
+import Settings from "./components/settings";
+import Friends from "./components/friends";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-sm-3">
+                    <div className="nav flex-column nav-pills" aria-orientation="vertical">
+                        <NavLink className="nav-link" to="/profile">
+                            Профиль
+                        </NavLink>
+                        <NavLink className="nav-link" to="messages">
+                            Сообщения
+                        </NavLink>
+                        <NavLink className="nav-link" to="settings" role="tab">
+                            Настройки
+                        </NavLink>
+                        <NavLink className="nav-link" to="friends">
+                            Друзья
+                        </NavLink>
+                    </div>
+                </div>
+                <div className="col-sm-9">
+                    <Routes>
+                        <Route path={'/profile'} element={<Profile/>} />
+                        <Route path={'/messages'} element={<Messages/>} />
+                        <Route path={'/settings'} element={<Settings/>} />
+                        <Route path={'/friends'} element={<Friends/>} />
+                    </Routes>
+                </div>
+            </div>
+        </div>
+
+
+    );
 }
 
 export default App;
